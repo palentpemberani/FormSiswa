557ff190d8a739bf7e55c47985567f13ea9d9374
palentpemberani <118250099+palentpemberani@users.noreply.github.com>
2023-07-20 11:47:29 +0700

    Add files via upload

---
+++ valent/src/valent/Valent.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package valent;
+
+/**
+ *
+ * @author LAB RPL
+ */
+public class Valent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
